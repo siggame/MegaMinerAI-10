eb6f18d871323afe0ac3f2d03ec46a1f5620f6ca
Jacob Fischer <jfischer@Jacob-PC.(none)>
2012-10-25 17:12:23 -0500

    Someone did something weird with the creature animation. Changed resources file so visualizer still works. Bryce/Anton will need to fix this at code sprint tomorrow.

--- plugins/galapagos/resources.r
+++ plugins/galapagos/resources.r
@@ -3,5 +3,6 @@ font Roboto textures/fonts/Roboto.png Roboto.dat
 texture grass textures/grass.png
 texture leaf textures/leaf.png
 texture sand textures/sand.jpg 
-animation creatureani textures/body_set.png
+texture animation_creatureani textures/set_body.png
+texture creatureani textures/pacman.png
 animation death textures/death.png
