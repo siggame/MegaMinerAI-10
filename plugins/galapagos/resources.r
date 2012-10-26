e2e6674bf0a2ea5305f9afa47279b21f147bc577
Bryce <bm4g5@mst.edu>
2012-10-25 19:10:34 -0500

    * Fixed Sprite Animations with new textures * In .meta files for the animations, the number of rows and columns specifies the whole texture rather than of the frames actually used

--- plugins/galapagos/resources.r
+++ plugins/galapagos/resources.r
@@ -3,6 +3,5 @@ font Roboto textures/fonts/Roboto.png Roboto.dat
 texture grass textures/grass.png
 texture leaf textures/leaf.png
 texture sand textures/sand.jpg 
-texture animation_creatureani textures/set_body.png
-texture creatureani textures/pacman.png
+animation creatureani textures/set_body.png
 animation death textures/death.png
