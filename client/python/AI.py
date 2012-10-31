e01b8b6cf5bbd0929329cfe625c704aca0631077
Justin Kellogg <jdk998@mst.edu>
2012-10-31 10:44:21 -0500

    shell ai for python

--- client/python/AI.py
+++ client/python/AI.py
@@ -23,19 +23,20 @@ class AI(BaseAI):
   ##This function is called each time it is your turn
   ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
-    mine = [creature for creature in self.creatures if creature.owner == self.playerID]
-    for creature in mine:
-      if not isinstance(self.getPlantAtLocation(creature.x+1,creature.y),Plant) and not isinstance(self.getCreatureAtLocation(creature.x+1,creature.y),Creature):
-        if (0<creature.x+1<self.mapWidth) and (0<creature.y<self.mapHeight) and creature.energyLeft>self.energyPerAction:
+    for creature in self.creatures:
+     if creature.owner == self.playerID:
+      plant = self.getPlantAtLocation(creature.x+1,creature.y)
+      if (plant == None or (isinstance(plant,Plant) and plant.size==0)) and self.getCreatureAtLocation(creature.x+1,creature.y)==None:
+        if (0<=creature.x+1<self.mapWidth) and (0<=creature.y<self.mapHeight) and creature.energyLeft>=self.energyPerAction and creature.movementLeft>0:
           creature.move(creature.x+1,creature.y)
       plant = self.getPlantAtLocation(creature.x+1,creature.y)
       creat = self.getCreatureAtLocation(creature.x+1,creature.y)
       if creature.canEat:
-        if isinstance(plant,Plant) and plant.size>0 and creature.energyLeft >= self.energyPerAction:
+        if plant != None and plant.size>0 and creature.energyLeft >= self.energyPerAction:
           creature.eat(plant.x,plant.y)
-        elif isinstance(creat,Creature) and creat.owner != self.playerID and creature.energyLeft >= self.energyPerAction:
+        elif creat != None and creat.owner != self.playerID and creature.energyLeft >= self.energyPerAction:
           creature.eat(creat.x,creat.y)
-      elif isinstance(creat,Creature) and creat.owner == self.playerID:
+      elif creat != None and creat.owner == self.playerID:
         if  creature.energyLeft>=self.energyPerBreed and creat.energyLeft>=self.energyPerBreed:
           if creature.canBreed and creat.canBread:
             creature.breed(creat)
